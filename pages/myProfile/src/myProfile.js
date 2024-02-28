8d39605653f0630ec5f1625be741fbb534b1be29
Neelesh Singh <singh.neelesh@apnibus.com>
2024-02-28 23:04:46 +0530

    refactore, added back buttons etc

--- pages/myProfile/src/myProfile.js
+++ pages/myProfile/src/myProfile.js
@@ -1,5 +1,5 @@
 // Load translations from JSON file
-const translations = fetch("my-profile-translations.json")
+const translations = fetch("myProfileTranslations.json")
   .then((response) => response.json())
   .catch((error) => console.error("Error loading translations:", error));
 
@@ -24,3 +24,14 @@ async function changeLanguage(languageCode) {
 
 // Set the default language here
 changeLanguage(localStorage.getItem("languageSelected") ?? "en");
+
+// Back Button to navigate to home page directly
+document.getElementById("backButton").addEventListener("click", function() {
+
+  // Navigate to the first page (index.html in this example)
+  window.location.href = "../../../index.html";
+
+  // Clear browsing history
+  window.history.replaceState(null, null, "../../../index.html");
+
+});
